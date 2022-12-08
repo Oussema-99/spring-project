7cc4f60f78e1f983f690ff319613dc6232a34a37
Oussema-99 <oussama.ghorbel@insat.u-carthage.tn>
2022-12-08 07:34:27 -0800

    added jenkinsfile

---
+++ simple-springboot-app/script.groovy
@@ -0,0 +1,12 @@
+def pushToNexus() {
+    echo "pushing the jar file to Nexus maven-snapshots repo..."
+    sh 'mvn clean package -D maven.test.skip=true deploy'
+}
+
+def sonarScan() {
+    echo "Running sonarQube scan..."
+    sh 'mvn test'
+    sh 'mvn sonar:sonar -D sonar.login=sqp_3c04da6ccdef164d7564e419573a13f984fa49bb'
+}
+
+return this
